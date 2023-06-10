a7bf4c832f83b9e67616ffd4b9eff5afa2a2cc0c
lopestorm <guillopes2002@gmail.com>
2023-06-10 20:45:33 -0300

    Update and rename twrp_M23.mk to twrp_M23H.mk

--- twrp_M23H.mk
+++ twrp_M23H.mk
@@ -14,10 +14,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from M23 device
-$(call inherit-product, device/doogee/M23/device.mk)
+$(call inherit-product, device/doogee/M23H/device.mk)
 
-PRODUCT_DEVICE := M23
-PRODUCT_NAME := twrp_M23
+PRODUCT_DEVICE := M23H
+PRODUCT_NAME := twrp_M23H
 PRODUCT_BRAND := DOOGEE
-PRODUCT_MODEL := V30
+PRODUCT_MODEL := V Max
 PRODUCT_MANUFACTURER := DOOGEE
